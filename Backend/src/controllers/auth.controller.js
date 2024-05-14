1a7a968a16e33a55b080b671a583449bb9ad6500
Byron <58144346+LegenB@users.noreply.github.com>
2024-05-13 20:06:59 -0400

    ID unicos

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -10,30 +10,38 @@ export const register = async (req, res) => {
 
     try {
         const emailExists = await EmailExist(email);
+        
 
-
-        if (emailExists) {
+        if (emailExists[0]) {
            
             res.status(400).send("El correo electrónico ya está registrado");
         } 
         else {
+            
+
             try {
+                //Generar un iD unico
+                const NombreID = (nombre.substr(0,3)).toUpperCase()
+                console.log(NombreID)
+                const newID = (emailExists[1] + 1 +`_'${NombreID}'`)
+
                 // Encriptar contraseña
+                console.log(newID)
                 const passwordHashs = await bcrypt.hash(password,10);
 
                 const response = await Conectclient.execute(
                 `INSERT INTO usuario (id_usuario, nombre, email, password, fecha_nacimiento, estado, chatglobal_id_chat_global) 
-                VALUES (03, '${nombre}', '${email}', '${passwordHashs}', '${fecha_nacimiento}', '${estado}', '001')`)
+                VALUES ('${newID}', '${nombre}', '${email}', '${passwordHashs}', '${fecha_nacimiento}', '${estado}', '001')`)
                 //console.log(response)
                 res.send("Registrado")
               
             } 
              catch (error)  {
                 console.error("Error al ejecutar la consulta:", error);
-                res.send("Error el id ")
+                res.send("Error el id ya existe ")
             }
-  
         }
+        
     } catch (error) {
         console.error("Error:", error)
     } 
--- Backend/src/middlewares/auth.register.js
+++ Backend/src/middlewares/auth.register.js
@@ -4,7 +4,7 @@ import { Conectclient } from "../DataBase.js";
 export async function EmailExist(email) {
     const Emails = await Conectclient.execute(`SELECT email FROM usuario`);
     const DB_Emails = Emails.rows;
-
+    const newID = DB_Emails.length
     let exist = false;
 
     DB_Emails.forEach(e => {
@@ -13,5 +13,5 @@ export async function EmailExist(email) {
         }
     });
 
-    return exist;
+    return [exist, newID];
 }
