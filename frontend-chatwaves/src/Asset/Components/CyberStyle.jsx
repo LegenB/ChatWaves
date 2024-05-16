dce52800b3c2498f2fb687a7353296a8a77add1a
Byron <58144346+LegenB@users.noreply.github.com>
2024-05-16 19:06:29 -0400

    bug of color fixed

--- frontend-chatwaves/src/Asset/Components/CyberStyle.jsx
+++ frontend-chatwaves/src/Asset/Components/CyberStyle.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export const CyberStyle = ({color, puntos = 4}) => {
-    let dot = ''
+export const CyberStyle = ({color}) => {
+
 
     let bg_color =''
     let br_color =''
     switch (color) {
         case 'blue':
-            bg_color ='cyber-blue/50'
+            bg_color ='bg-cyber-blue/50'
             br_color = 'border-cyber-blue/50'
             break;
     
         default:
-            bg_color ='cyber-pink/50'
+            bg_color ='bg-cyber-pink/50'
             br_color = 'border-cyber-pink/50'
             break;
     }
@@ -23,23 +23,23 @@ export const CyberStyle = ({color, puntos = 4}) => {
         <>
             
 
-            <span className={`absolute  bg-${bg_color} w-2  h-2 rounded-full z-20 top-2 left-5 `}></span>
-            <span className={` absolute bg-${bg_color} w-2  h-2 rounded-full z-20 top-2 left-10 `}></span>
-            <span className={` absolute bg-${bg_color} w-2  h-2 rounded-full z-20 top-2 left-[60px] `}></span>
-            <span className={` absolute bg-${bg_color} w-2  h-2 rounded-full z-20 top-2 left-[80px]`}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[23px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[36px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[49px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[62px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[75px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[88px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[101px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[114px] left-2 `}></span>
-            <span className={` absolute bg-${bg_color} w-8  h-2  z-20 bottom-[10px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-2  h-2 rounded-full z-20 top-2 left-5 `}></span>
+            <span className={`absolute ${bg_color} w-2  h-2 rounded-full z-20 top-2 left-10 `}></span>
+            <span className={`absolute ${bg_color} w-2  h-2 rounded-full z-20 top-2 left-[60px] `}></span>
+            <span className={`absolute ${bg_color} w-2  h-2 rounded-full z-20 top-2 left-[80px]`}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[23px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[36px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[49px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[62px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[75px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[88px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[101px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[114px] left-2 `}></span>
+            <span className={`absolute ${bg_color} w-8  h-2  z-20 bottom-[10px] left-2 `}></span>
 
             <span className={`absolute bottom-2 right-2 w-0 h-0 border-l-[45px] border-l-transparent border-r-[0px] border-r-transparent border-b-[45px] ${br_color} `}></span>
             
-            <span className={`absolute top-1/2 right-2 transform -translate-y-1/2 bg-${bg_color}  rounded-full w-1 h-2/5 z-20 `}></span>
+            <span className={`absolute top-1/2 right-2 transform -translate-y-1/2 ${bg_color}  rounded-full w-1 h-2/5 z-20 `}></span>
         </>
     )
 }
