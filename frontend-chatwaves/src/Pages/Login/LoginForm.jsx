d10467268612d76c0f92b4e59947e997f249b194
Cristian Tapia <cristianxd2022@gmail.com>
2024-05-16 19:40:58 -0400

    Update Login Design - CyberStyle Apply

--- frontend-chatwaves/src/Pages/Login/LoginForm.jsx
+++ frontend-chatwaves/src/Pages/Login/LoginForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { CyberStyle } from "../../Asset/Components/CyberStyle";
 export const LoginForm = () => {
     const bg_image = 'https://i.ibb.co/CMZ8zt5/Fondo-Min.png'
     
@@ -11,8 +11,9 @@ export const LoginForm = () => {
                         <img src={bg_image} alt="Background" className="object-cover w-full h-full brightness-90" />
                 </div>
                 <div className="absolute inset-0 z-10 bg-transparent"></div>
-                <div className='absolute h-[600px] w-[500px] bg-slate-500 drop-shadow-[0_5px_35px_rgba(34,235,242,0.50)] text-[rgb(34,235,242)] bg-opacity-60 rounded-xl backdrop-blur-sm flex justify-center p-10'></div>
-                <div className='z-20 h-[600px] w-[500px] flex justify-center p-10'>
+                <div className='absolute h-[600px] w-[500px] bg-zinc-900/95 drop-shadow-[0_5px_35px_rgba(34,235,242,0.50)] text-[rgb(34,235,242)] rounded-xl flex justify-center p-10'></div>
+                <div className='relative z-20 h-[600px] w-[500px] p-10'>
+                <CyberStyle color={'#e753f5'}/>
                     <div className='w-full h-full'>
                         <div className='flex justify-center'>
                             <span className='text-center font-Saiba text-5xl drop-shadow-[0_5px_35px_rgba(34,235,242,1)] text-[#22ebf2] mb-10'>L</span>
@@ -24,7 +25,12 @@ export const LoginForm = () => {
                             <form className='text-center'>
                                 <input type="email" placeholder="Email..." className='w-60 font-Neuron shadow-md shadow-[rgb(34,235,242)] border-b rounded-lg px-2 border-green-400 bg-transparent focus:outline-none hover:shadow-lg hover:shadow-[rgb(34,235,242)] transition'/>
                                 <input type="password" placeholder="Password..." className='w-60 font-Neuron shadow-md shadow-[rgb(34,235,242)] mt-14 border-b rounded-lg px-2 border-green-500 bg-transparent focus:outline-none hover:shadow-lg hover:shadow-[rgb(34,235,242)] transition'/>
-                                <a href="/" className='flex justify-end mt-6 pr-24 text-sm font-Neuron'>Forgot your password?</a>
+                                <div className='flex justify-start pl-24 mt-5'>
+                                    <input type="checkbox" className='flex justify-start m'/>
+                                    <label htmlFor="" className='pl-2 text-white font-Neuron'>Remember me</label>
+                                </div>
+                                <a href="/" className='flex justify-center pt-20 text-sm text-white font-Neuron'>Forgot your password?</a>
+                                
                                 <button className='button_login'>Sign In</button>
                             </form>
                         </div>
